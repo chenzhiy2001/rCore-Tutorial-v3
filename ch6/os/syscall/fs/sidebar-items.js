88bf871e6a2bcd60b098626a7eeb8a964f290463
xishine <xishine@users.noreply.github.com>
2023-05-06 13:18:24 +0000

    deploy: cadd9257b0165c7ac7d171264c0efa73e7afa520

--- ch6/os/syscall/fs/sidebar-items.js
+++ ch6/os/syscall/fs/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["FD_STDIN",""],["FD_STDOUT",""]],"fn":[["sys_read",""],["sys_write",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["sys_close",""],["sys_open",""],["sys_read",""],["sys_write",""]]};
